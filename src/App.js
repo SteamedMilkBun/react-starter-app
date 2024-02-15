0a692bb0b37e4f52eb8ea56fa953b44c0c6cb8a3
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2024-02-15 16:39:11 -0600

    Added instructions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-
-
 import './App.css';
+
+// Create varying state.
+  // 1. with the array below - hard code out a piece of state called todos. Figure out a way to loop through the todos and render then to the page.
+  // const todos = ['wash car', 'call mom', 'make money']
+
+  // 2. Create a piece of state called count.
+    // Create buttons that will increase and decrease the count based off which is clicked.
+    // If the count gets below -10 or above 10, it resets to 0
+    
 
 
 
